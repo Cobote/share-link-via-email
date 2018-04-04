ef6359aab2089eeeab90ae007c99ae0912e99be8
Robin Smith <robin99smith@gmail.com>
2018-04-04 20:44:51 +0100

    Grunt babel

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,28 @@
-module.exports = function (grunt) {
+module.exports = function gruntFn(grunt) {
   grunt.initConfig({
     eslint: {
-      src: ['src/**/*.js']
-    }
+      src: ['src/**/*.js'],
+    },
+    babel: {
+      options: {
+        sourceMap: true,
+        presets: ['env'],
+      },
+      dist: {
+        files: {
+          'build_dev/functions.js': 'src/functions.js',
+        },
+      },
+    },
+    watch: {
+      files: ['<%= eslint.src %>'],
+      tasks: ['babel'],
+    },
   });
 
   grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['eslint']);
+  grunt.registerTask('default', ['babel']);
 };
