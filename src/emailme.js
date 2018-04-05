b98fcc21cd2395fb73184b5d3ba3223f9fa4f8eb
Robin Smith <robin99smith@gmail.com>
2018-04-05 19:44:02 +0100

    eslint emailme

--- src/emailme.js
+++ src/emailme.js
@@ -1,3 +1,4 @@
+/* global chrome */
 // Creates each of the links to be used by each type of Email client
 localStorage.mailOptionsLength = 6;
 
@@ -31,156 +32,160 @@ if (!firstRun) {
 const mailOptions = getOptions();
 
 
-var favoriteMailto = mailOptions["mail_picker_1"];
-var favoriteGmail = mailOptions["mail_picker_2"];
-var favoriteHotmail = mailOptions["mail_picker_3"];
-var favoriteYmail = mailOptions["mail_picker_4"];
-var favoriteAOL = mailOptions["mail_picker_5"];
-var favoriteInbox = mailOptions["mail_picker_6"];
+const favoriteMailto = mailOptions.mail_picker_1;
+const favoriteGmail = mailOptions.mail_picker_2;
+const favoriteHotmail = mailOptions.mail_picker_3;
+const favoriteYmail = mailOptions.mail_picker_4;
+const favoriteAOL = mailOptions.mail_picker_5;
+const favoriteInbox = mailOptions.mail_picker_6;
 
-var newWindowMailto = mailOptions["new_window_1"];
-var newWindowGmail = mailOptions["new_window_2"];
-var newWindowHotmail = mailOptions["new_window_3"];
-var newWindowYmail = mailOptions["new_window_4"];
-var newWindowAOL = mailOptions["new_window_5"];
-var newWindowInbox = mailOptions["new_window_6"];
+const newWindowMailto = mailOptions.new_window_1;
+const newWindowGmail = mailOptions.new_window_2;
+const newWindowHotmail = mailOptions.new_window_3;
+const newWindowYmail = mailOptions.new_window_4;
+const newWindowAOL = mailOptions.new_window_5;
+const newWindowInbox = mailOptions.new_window_6;
 
-var toEmailAdd = mailOptions["mail_to"];
+const toEmailAdd = mailOptions.mail_to;
 
-var beforeMsg = mailOptions["mail_before"];
-console.log("before message: " + beforeMsg);
+const beforeMsg = mailOptions.mail_before;
+// console.log("before message: " + beforeMsg);
 
-var afterMsg = mailOptions["mail_after"];
-console.log("after message: " + afterMsg);
+const afterMsg = mailOptions.mail_after;
+// console.log("after message: " + afterMsg);
 
-var newLineAfter = mailOptions["newLineAfter"];
-var newLineAfterNum = mailOptions["newLineAfterNum"];
-var newLineBefore = mailOptions["newLineBefore"];
-var newLineBeforeNum = mailOptions["newLineBeforeNum"];
+const [newLineAfter] = mailOptions;
+const [newLineAfterNum] = mailOptions;
+const [newLineBefore] = mailOptions;
+const [newLineBeforeNum] = mailOptions;
 
-var context;
-var title;
-var id;
+let context;
+let title;
+let id;
 
 // Create email menu option for each context type in this order
 if (favoriteMailto === 'true') {
-    createContext('Email', emailLink);
+  createContext('Email', emailLink);
 }
 if (favoriteAOL === 'true') {
-    createContext('AOL Mail', aolLink);
+  createContext('AOL Mail', aolLink);
 }
 if (favoriteGmail === 'true') {
-    createContext('Gmail', gmailLink);
+  createContext('Gmail', gmailLink);
 }
 if (favoriteInbox === 'true') {
-    createContext('Inbox', inboxLink);
+  createContext('Inbox', inboxLink);
 }
 if (favoriteHotmail === 'true') {
-    createContext('Outlook.com', hotmailLink);
+  createContext('Outlook.com', hotmailLink);
 }
 if (favoriteYmail === 'true') {
-    createContext('Yahoo! Mail', ymailLink);
+  createContext('Yahoo! Mail', ymailLink);
 }
 
 // Check if only one option selected
 if (getOptionsShownCount() === 1) {
-  chrome.browserAction.setPopup({ popup: ''});
-  chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.browserAction.setPopup({ popup: '' });
+  chrome.browserAction.onClicked.addListener((tab) => {
     open_email_handler(getSingleOptionInt());
   });
 } else {
-    chrome.browserAction.setPopup({ popup: 'popup.html'});
+  chrome.browserAction.setPopup({ popup: 'popup.html'});
 }
 
 // get link
 function getLink(info, tab) {
+  let pageUrl;
+
   if (info.linkUrl) {
-	// context if link
-	pageUrl = info.linkUrl;
+    // context if link
+    pageUrl = info.linkUrl;
   } else {
-	pageUrl = tab.url;
+    pageUrl = tab.url;
   }
-  //pageUrl = encodeURIComponent(pageUrl);
-  console.log("page url: " + pageUrl);
+  // pageUrl = encodeURIComponent(pageUrl);
+  // console.log("page url: " + pageUrl);
   return pageUrl;
 }
 
 // get title
 function getTitle(info, tab) {
+  let pageTitle;
+
   if (info.linkUrl) {
-	// context if link
-	pageTitle = '';
+    // context if link
+    pageTitle = '';
   } else {
-	pageTitle = tab.title;
+    pageTitle = tab.title;
   }
-  console.log("page title: " + pageTitle);
+  // console.log("page title: " + pageTitle);
   return pageTitle;
 }
 
 // create a new email
 function emailLink(info, tab) {
-    var mailsrvr = '';
-    var newLineChar = '%0A';
-    var newWindow = newWindowMailto;
-    var mailTo = toEmailAdd;
+  let mailsrvr = '';
+  const newLineChar = '%0A';
+  const newWindow = newWindowMailto;
+  const mailTo = toEmailAdd;
 
-    mailsrvr = 'mailto:' + mailTo + '?Subject=';
-    createEmailTab(info, tab, mailsrvr, newLineChar, newWindow);
+  mailsrvr = `mailto:${mailTo}?Subject=`;
+  createEmailTab(info, tab, mailsrvr, newLineChar, newWindow);
 }
 
 // create a new Gmail
 function gmailLink(info, tab) {
-    var mailsrvr = '';
-    var newLineChar = '%0A';
-    var newWindow = newWindowGmail;
-    var mailTo = toEmailAdd;
+  let mailsrvr = '';
+  const newLineChar = '%0A';
+  const newWindow = newWindowGmail;
+  const mailTo = toEmailAdd;
 
-    mailsrvr = 'https://mail.google.com/mail/?view=cm&fs=1&tf=1&=1&to=' + mailTo + '&su=';
-    createEmailTab(info, tab, mailsrvr, newLineChar, newWindow);
+  mailsrvr = `https://mail.google.com/mail/?view=cm&fs=1&tf=1&=1&to=${mailTo}&su=`;
+  createEmailTab(info, tab, mailsrvr, newLineChar, newWindow);
 }
 
 // create a new hotmail
 function hotmailLink(info, tab) {
-    var mailsrvr = '';
-    var newLineChar = '%0A';
-    var newWindow = newWindowHotmail;
-    var mailTo = toEmailAdd;
+  let mailsrvr = '';
+  const newLineChar = '%0A';
+  const newWindow = newWindowHotmail;
+  const mailTo = toEmailAdd;
 
-    // mailsrvr = 'http://mail.live.com/?rru=compose&to=' + mailTo + '&subject=';
-    mailsrvr = 'https://outlook.live.com/owa/?path=/mail/action/compose&to=' + mailTo + '&subject=';
-    createEmailTab(info, tab, mailsrvr, newLineChar, newWindow);
+  // mailsrvr = 'http://mail.live.com/?rru=compose&to=' + mailTo + '&subject=';
+  mailsrvr = `https://outlook.live.com/owa/?path=/mail/action/compose&to=${mailTo}&subject=`;
+  createEmailTab(info, tab, mailsrvr, newLineChar, newWindow);
 }
 
 // create a new ymail
 function ymailLink(info, tab) {
-    var mailsrvr = '';
-    var newLineChar = '%0A';
-    var newWindow = newWindowYmail;
-    var mailTo = toEmailAdd;
+  let mailsrvr = '';
+  const newLineChar = '%0A';
+  const newWindow = newWindowYmail;
+  const mailTo = toEmailAdd;
 
-    mailsrvr = 'http://compose.mail.yahoo.com?to=' + mailTo + '&subject=';
-    //mailsrvr = 'https://us-mg40.mail.yahoo.com/neo/launch?action=compose&to=' + mailTo + '&subj=';
-    createEmailTab(info, tab, mailsrvr, newLineChar, newWindow);
+  mailsrvr = `http://compose.mail.yahoo.com?to=${mailTo}&subject=`;
+  // mailsrvr = 'https://us-mg40.mail.yahoo.com/neo/launch?action=compose&to=' + mailTo + '&subj=';
+  createEmailTab(info, tab, mailsrvr, newLineChar, newWindow);
 }
 
 // create a new AOL mail
 function aolLink(info, tab) {
-    var mailsrvr = '';
-    var newLineChar = '%20'; //using space
-    var newWindow = newWindowAOL;
-    var mailTo = toEmailAdd;
+  let mailsrvr = '';
+  const newLineChar = '%20'; // using space
+  const newWindow = newWindowAOL;
+  const mailTo = toEmailAdd;
 
-    mailsrvr = 'https://mail.aol.com/mail/ComposeMessage.aspx?to=' + mailTo + '&subject=';
-    createEmailTab(info, tab, mailsrvr, newLineChar, newWindow);
+  mailsrvr = `https://mail.aol.com/mail/ComposeMessage.aspx?to=${mailTo}&subject=`;
+  createEmailTab(info, tab, mailsrvr, newLineChar, newWindow);
 }
 
 // create a new Inbox
 function inboxLink(info, tab) {
-    var mailsrvr = '';
-    var newLineChar = '%0A';
-    var newWindow = newWindowInbox;
-    var mailTo = toEmailAdd;
+  let mailsrvr = '';
+  const newLineChar = '%0A';
+  const newWindow = newWindowInbox;
+  const mailTo = toEmailAdd;
 
-	mailsrvr = 'https://inbox.google.com/u/0/?to=' + mailTo + '&subject=';
-    createEmailTab(info, tab, mailsrvr, newLineChar, newWindow);
+  mailsrvr = `https://inbox.google.com/u/0/?to=${mailTo}&subject=`;
+  createEmailTab(info, tab, mailsrvr, newLineChar, newWindow);
 }
