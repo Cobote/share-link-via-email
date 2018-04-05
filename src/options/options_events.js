2bc0a9ce9e30cb7ee5f06ed0498e061940c5f116
Robin Smith <robin99smith@gmail.com>
2018-04-05 20:52:21 +0100

    eslint options

--- src/options/options_events.js
+++ src/options/options_events.js
@@ -30,59 +30,60 @@ function newLineBeforeClickHandler() {
 
   getPreview();
 }
-function btn_reset_clickHandler(e) {
-    restore_options();
-    getPreview();
+function btnResetClickHandler() {
+  restore_options();
+  getPreview();
 }
-function mail_picker_0_clickHandler(e) {
-    changeAll(this, "mail_picker");
-    save_sender_options();
+function mailPicker0ClickHandler() {
+  changeAll(this, 'mail_picker');
+  save_sender_options();
 }
-function mail_picker_clickHandler(e) {
-    changeCheck("mail_picker");
-    save_sender_options();
+function mailPickerClickHandler() {
+  changeCheck('mail_picker');
+  save_sender_options();
 }
-function new_window_0_clickHandler(e) {
-    changeAll(this, "new_window");
-    save_sender_options();
+function newWindow0ClickHandler() {
+  changeAll(this, 'new_window');
+  save_sender_options();
 }
-function new_window_clickHandler(e) {
-    changeCheck("new_window");
-    save_sender_options();
+function newWindowClickHandler() {
+  changeCheck('new_window');
+  save_sender_options();
 }
 
 // Add event listeners once the DOM has fully loaded by listening for the
 // `DOMContentLoaded` event on the document, and adding your listeners to
 // specific elements when it triggers.
-document.addEventListener('DOMContentLoaded', function() {
-	var mailOptionsLength = localStorage['mailOptionsLength'];
-    // on load events
-    restore_options();
-    getPreview();
-    // end on load
+document.addEventListener('DOMContentLoaded', () => {
+  const [mailOptionsLength] = localStorage;
+  let i;
 
-    // listeners
-    $('#btn_save').click(btn_save_clickHandler);
-    $('#btn_restore').click(btn_reset_clickHandler);
-    $('#newLineAfter').change(newLineAfter_clickHandler);
-    $('#newLineBefore').change(newLineBefore_clickHandler);
-    $('#newLineAfterNum').keyup(getPreview);
-    $('#newLineBeforeNum').keyup(getPreview);
-    $('#mail_before').keyup(getPreview);
-    $('#mail_after').keyup(getPreview);
+  // on load events
+  restore_options();
+  getPreview();
+  // end on load
 
-    $('#mail_picker_0').change(mail_picker_0_clickHandler);
-	for (var i = 1; i <= mailOptionsLength; i++) {
-		$('#mail_picker_' + i).change(mail_picker_clickHandler);
-	}
+  // listeners
+  $('#btn_save').click(btn_save_clickHandler);
+  $('#btn_restore').click(btn_reset_clickHandler);
+  $('#newLineAfter').change(newLineAfter_clickHandler);
+  $('#newLineBefore').change(newLineBefore_clickHandler);
+  $('#newLineAfterNum').keyup(getPreview);
+  $('#newLineBeforeNum').keyup(getPreview);
+  $('#mail_before').keyup(getPreview);
+  $('#mail_after').keyup(getPreview);
 
-    $('#new_window_0').change(new_window_0_clickHandler);
-	for (var i = 1; i <= mailOptionsLength; i++) {
-		$('#new_window_' + i).change(new_window_clickHandler);
-	}
+  $('#mail_picker_0').change(mail_picker_0_clickHandler);
+  for (i = 1; i <= mailOptionsLength; i += 1) {
+    $(`#mail_picker_${i}`).change(mail_picker_clickHandler);
+  }
 
-    // stop normal form submission
-    $( "form" ).submit(function( event ) {
-	  event.preventDefault();
-	});
+  $('#new_window_0').change(new_window_0_clickHandler);
+  for (i = 1; i <= mailOptionsLength; i += 1) {
+    $(`#new_window_${i}`).change(new_window_clickHandler);
+  }
+
+  // stop normal form submission
+  const form = document.getElementById('optionsForm');
+  form.onsubmit = () => false;
 });
