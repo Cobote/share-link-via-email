eaef8ca04739f9419027de932ef4e17867c047fd
Robin Smith <robin99smith@gmail.com>
2018-04-16 20:28:02 +0100

    Check element exists before changing style

--- src/popup/showHideOptions.js
+++ src/popup/showHideOptions.js
@@ -7,16 +7,25 @@ function showHideOptions() {
   let select;
   let mailtype;
   let i;
-
   const mailOptions = getOptions();
+  let isOptionSet = false;
 
   // Hide options not selected
   for (i = 0; i <= mailOptionsLength; i += 1) {
     mailtype = mailOptions[`mail_picker_${i}`];
     select = document.getElementById(`mail_picker_${i}`);
-    if (mailtype !== 'true') {
-      select.style.display = 'none';
+    if (select) {
+      if (mailtype !== 'true') {
+        select.style.display = 'none';
+      } else {
+        isOptionSet = true;
+      }
     }
+  }
+
+  // Check if no options have been selected
+  if (!isOptionSet) {
+    document.getElementById('mail_picker_none').style.display = 'inline';
   }
 }
 
