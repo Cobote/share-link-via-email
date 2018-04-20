258c50c995b68047050dfe26178ccbdc574ff36a
Robin Smith <robin99smith@gmail.com>
2018-04-20 20:10:26 +0100

    LF ending

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = merge(common, {
   devtool: 'source-map',
   output: {
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, 'build'),
   },
   plugins: [
     new CleanWebpackPlugin(['build']),
